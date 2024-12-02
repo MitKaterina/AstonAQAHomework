db940b7a621f650e3c4336fbb72f0619c5b2daf9
MitKaterina <ekaterina.voropay@gmail.com>
2024-12-02 20:39:18 +0300

    Add HomeWork 14

--- src/test/java/HomeWork14/FactorialCalculatorTest.java
+++ src/test/java/HomeWork14/FactorialCalculatorTest.java
@@ -1,26 +1,33 @@
 package HomeWork14;
 
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-class FactorialCalculatorTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
+public class FactorialCalculatorTest {
+
+    @DisplayName("Тестирование вычисления факториала для положительных чисел")
     @Test
-    void testFactorialOfPositiveNumber() {
+    public void testFactorialOfPositiveNumber() {
         // Проверка факториала для положительного числа
         assertEquals(120, FactorialCalculator.calculateFactorial(5));
     }
 
+    @DisplayName("Тестирование факториала для нуля")
     @Test
-    void testFactorialOfZero() {
+    public void testFactorialOfZero() {
         // Проверка факториала для нуля (должно быть 1)
         assertEquals(1, FactorialCalculator.calculateFactorial(0));
     }
 
+    @DisplayName("Тестирование вычисления факториала исключения для отрицательного числа ")
     @Test
-    void testFactorialOfNegativeNumber() {
+    public void testFactorialOfNegativeNumber() {
         // Проверка факториала для отрицательного числа (должно выбросить исключение)
-        assertThrows(IllegalArgumentException.class, () -> FactorialCalculator.calculateFactorial(-5));
+        assertThrows(IllegalArgumentException.class, () -> FactorialCalculator.calculateFactorial(-1));
     }
+
 }
